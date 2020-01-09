de265892f1ccdc1f76f4abddb6a9911d2702ca35
unknown <1178515826@qq.com>
2020-01-09 18:39:48 +0800

    init pj

---
+++ user/service/src/main/java/com/gapache/user/service/ServiceApplication.java
@@ -0,0 +1,16 @@
+package com.gapache.user.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author HuSen
+ * create on 2020/1/8 18:01
+ */
+@SpringBootApplication
+public class ServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
+}
