0b706f455c608ade9fdbe5c9665214f886b7078d
unknown <1178515826@qq.com>
2020-01-11 09:57:12 +0800

    使用更好的AES算法

--- base/commons/src/main/java/com/gapache/commons/security/AESUtils.java
+++ base/commons/src/main/java/com/gapache/commons/security/AESUtils.java
@@ -4,7 +4,9 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
@@ -17,7 +19,7 @@ public class AESUtils {
     /**
      * 算法/模式/补码方式
      */
-    private static final String ALGORITHM = "AES/ECB/PKCS5Padding";
+    private static final String ALGORITHM = "AES/CBC/PKCS5Padding";
 
     private static final String KEY_ALGORITHM = "AES";
 
@@ -33,10 +35,10 @@ public class AESUtils {
      * @throws BadPaddingException       BadPaddingException
      * @throws IllegalBlockSizeException IllegalBlockSizeException
      */
-    public static byte[] encrypt(byte[] rawData, byte[] sKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+    public static byte[] encrypt(byte[] rawData, byte[] sKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException {
         SecretKeySpec keySpec = new SecretKeySpec(sKey, KEY_ALGORITHM);
         Cipher cipher = Cipher.getInstance(ALGORITHM);
-        cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, new IvParameterSpec(sKey));
         return cipher.doFinal(rawData);
     }
 
@@ -52,10 +54,10 @@ public class AESUtils {
      * @throws BadPaddingException       BadPaddingException
      * @throws IllegalBlockSizeException IllegalBlockSizeException
      */
-    public static byte[] decrypt(byte[] encodedData, byte[] sKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+    public static byte[] decrypt(byte[] encodedData, byte[] sKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException {
         SecretKeySpec keySpec = new SecretKeySpec(sKey, KEY_ALGORITHM);
         Cipher cipher = Cipher.getInstance(ALGORITHM);
-        cipher.init(Cipher.DECRYPT_MODE, keySpec);
+        cipher.init(Cipher.DECRYPT_MODE, keySpec, new IvParameterSpec(sKey));
         return cipher.doFinal(encodedData);
     }
 }
