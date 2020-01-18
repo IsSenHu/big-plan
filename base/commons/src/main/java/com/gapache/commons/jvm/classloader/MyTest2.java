220a62c9ffdd83d4b28b658787caef18b6e29697
husen <1178515826@qq.com>
2020-01-19 01:24:59 +0800

    ok

--- base/commons/src/main/java/com/gapache/commons/jvm/classloader/MyTest2.java
+++ base/commons/src/main/java/com/gapache/commons/jvm/classloader/MyTest2.java
@@ -14,10 +14,11 @@ public class MyTest2 {
         // 注意：这里指的是将常量存放到了MyTest2的常量池中，之后MyTest2与MyParent2就没有任何关系了
         // 甚至，我们可以将MyParent2的class问价删除
 
-        // 助记符：lcd表示将int，float或String类型的常量值从常量池中推送至栈顶
-        //        bipush表示将单字节（-128 ~ 127）的常量值推送至栈顶
-        //        sipush表示将一个短整型常量值（-32768 ~ 32767）推送至栈顶
-        //        iconst_1表示将int类型1推送至栈顶（iconst_m1 ~ iconst_5，-1 ~ 5）
+        // 助记符：lcd表示将int，float或String类型的常量值从常量池中推送至栈顶 类LDC
+        //        bipush表示将单字节（-128 ~ 127）的常量值推送至栈顶 类BIPUSH
+        //        sipush表示将一个短整型常量值（-32768 ~ 32767）推送至栈顶 类SIPUSH
+        //        iconst_1表示将int类型1推送至栈顶（iconst_m1 ~ iconst_5，-1 ~ 5）类ICONST
+
         // System.out.println(MyParent2.str);
         // 输出
         // hello world
