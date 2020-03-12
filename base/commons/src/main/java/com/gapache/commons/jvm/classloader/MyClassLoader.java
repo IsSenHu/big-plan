33cb44a94c1b9326c1307574d9bc8cd6df4bed99
IsSenHu <1178515826@qq.com>
2020-03-12 17:59:44 +0800

    in mac os change

--- base/commons/src/main/java/com/gapache/commons/jvm/classloader/MyClassLoader.java
+++ base/commons/src/main/java/com/gapache/commons/jvm/classloader/MyClassLoader.java
@@ -6,19 +6,19 @@ import java.io.*;
  * @author HuSen
  * create on 2020/1/25 13:15
  */
-public class MyTest14 extends ClassLoader {
+public class MyClassLoader extends ClassLoader {
 
     private String classDir;
     private String classLoaderName;
     private static final String fileExtension = ".class";
 
-    public MyTest14(ClassLoader parent, String classDir, String classLoaderName) {
+    public MyClassLoader(ClassLoader parent, String classDir, String classLoaderName) {
         super(parent);
         this.classDir = classDir;
         this.classLoaderName = classLoaderName;
     }
 
-    public MyTest14(String classDir, String classLoaderName) {
+    public MyClassLoader(String classDir, String classLoaderName) {
         this.classDir = classDir;
         this.classLoaderName = classLoaderName;
     }
@@ -49,44 +49,44 @@ public class MyTest14 extends ClassLoader {
 
     @Override
     public String toString() {
-        return "MyTest14{" +
+        return "MyClassLoader{" +
                 "classLoaderName='" + classLoaderName + '\'' +
                 '}';
     }
 
     public static void main(String[] args) throws Exception {
-        MyTest14 loader1 = new MyTest14("F:\\", "loader1");
+        MyClassLoader loader1 = new MyClassLoader("/Users/macos/Documents/codes/mine/game/target/classes/", "loader1");
         // 加载ClassPath下的类
         test(loader1, "com.gapache.commons.jvm.classloader.MyTest1");
-        // class: 325040804
+        // class: 1581781576
         // classLoader: sun.misc.Launcher$AppClassLoader@18b4aac2
-        // newInstance: com.gapache.commons.jvm.classloader.MyTest1@45ee12a7
+        // newInstance: com.gapache.commons.jvm.classloader.MyTest1@66d3c617
         System.out.println("------------------");
         // 加载指定classDir目录下的类
-        test(loader1, "com.example.clear.TestLoaderIt");
-        // class: 1265094477
-        // classLoader: MyTest14{classLoaderName='loader1'}
-        // newInstance: com.example.clear.TestLoaderIt@7ea987ac
+        test(loader1, "com.qdhh.game.MyEncrypt");
+        // class: 895328852
+        // classLoader: MyClassLoader{classLoaderName='loader1'}
+        // newInstance: com.qdhh.game.MyEncrypt@4dc63996
         System.out.println("------------------");
         // 再次使用不同的加载器加载同一个类
-        MyTest14 loader2 = new MyTest14("F:\\", "loader2");
+        MyClassLoader loader2 = new MyClassLoader("/Users/macos/Documents/codes/mine/game/target/classes/", "loader2");
         test(loader2, "com.gapache.commons.jvm.classloader.MyTest1");
-        // class: 1265094477
-        // classLoader: MyTest14{classLoaderName='loader1'}
-        // newInstance: com.example.clear.TestLoaderIt@7ea987ac
+        // class: 1581781576
+        // classLoader: sun.misc.Launcher$AppClassLoader@18b4aac2
+        // newInstance: com.gapache.commons.jvm.classloader.MyTest1@d716361
         System.out.println("------------------");
         // 再次使用不同的加载器加载classDir目录下的类
-        test(loader2, "com.example.clear.TestLoaderIt");
-        // class: 1846274136
-        // classLoader: MyTest14{classLoaderName='loader2'}
-        // newInstance: com.example.clear.TestLoaderIt@61bbe9ba
+        test(loader2, "com.qdhh.game.MyEncrypt");
+        // class: 1259475182
+        // classLoader: MyClassLoader{classLoaderName='loader2'}
+        // newInstance: com.qdhh.game.MyEncrypt@4d7e1886
         System.out.println("-------------------");
         // 使用loader1作为loader3的父加载器，加载claasDir目录下的类
-        MyTest14 loader3 = new MyTest14(loader1, "F:\\", "loader3");
-        test(loader3, "com.example.clear.TestLoaderIt");
-        // class: 1265094477
-        // classLoader: MyTest14{classLoaderName='loader1'}
-        // newInstance: com.example.clear.TestLoaderIt@610455d6
+        MyClassLoader loader3 = new MyClassLoader(loader1, "/Users/macos/Documents/codes/mine/game/target/classes/", "loader3");
+        test(loader3, "com.qdhh.game.MyEncrypt");
+        // class: 895328852
+        // classLoader: MyClassLoader{classLoaderName='loader1'}
+        // newInstance: com.qdhh.game.MyEncrypt@3cd1a2f1
     }
 
     public static void test(ClassLoader classLoader, String className) throws Exception {
