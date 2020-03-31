bad5286ae5d4025582802458bfad457b73a6fb50
IsSenHu <1178515826@qq.com>
2020-03-31 11:48:37 +0800

    change

--- base/commons/src/main/java/com/gapache/commons/jvm/bytecode/parse/attribute/AbstractAttributeInfo.java
+++ base/commons/src/main/java/com/gapache/commons/jvm/bytecode/parse/attribute/AbstractAttributeInfo.java
@@ -1,8 +1,13 @@
 package com.gapache.commons.jvm.bytecode.parse.attribute;
 
 import com.gapache.commons.jvm.bytecode.parse.ByteCode;
+import com.gapache.commons.jvm.bytecode.parse.cp.ConstantItem;
+import com.gapache.commons.jvm.bytecode.parse.cp.IndexConstantItem;
+import com.gapache.commons.jvm.bytecode.parse.cp.ValueConstantItem;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Map;
 
 /**
  * @author HuSen
@@ -18,4 +23,15 @@ public abstract class AbstractAttributeInfo {
     public abstract void parsing(ByteCode byteCode);
 
     public abstract void printing(ByteCode byteCode);
+
+    protected String checkClassName(Map<Integer, ConstantItem> constantPool, int index) {
+        IndexConstantItem classInfo = (IndexConstantItem) constantPool.get(index);
+        ValueConstantItem className = (ValueConstantItem) constantPool.get(classInfo.getIndexes()[0]);
+        return className.getValue().toString();
+    }
+
+    protected String checkUtf8Value(Map<Integer, ConstantItem> constantPool, int index) {
+        ValueConstantItem utf8Info = (ValueConstantItem) constantPool.get(index);
+        return utf8Info.getValue().toString();
+    }
 }
--- base/commons/src/main/java/com/gapache/commons/jvm/bytecode/parse/attribute/BootstrapMethodsAttributeInfo.java
+++ base/commons/src/main/java/com/gapache/commons/jvm/bytecode/parse/attribute/BootstrapMethodsAttributeInfo.java
@@ -1,26 +1,50 @@
 package com.gapache.commons.jvm.bytecode.parse.attribute;
 
+import com.gapache.commons.jvm.bytecode.parse.AbstractTableView;
 import com.gapache.commons.jvm.bytecode.parse.ByteCode;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author HuSen
  * create on 2020/3/30 4:53 下午
  */
-public class BootstrapMethodsAttributeInfo extends AbstractAttributeInfo {
-    private Integer attributeNameIndex;
-    private Integer attributeLength;
-    private Integer numBootstrapMethods;
-    private Map<Integer, BootstrapMethod> bootstrapMethods;
+public class BootstrapMethodsAttributeInfo extends AbstractTableView {
+
+    private List<List<String>> trs;
 
     @Override
     public void parsing(ByteCode byteCode) {
-
+        String hexString = super.getInfo().toString();
+        int rows = rows(hexString);
+        int point = 4;
+        trs = new ArrayList<>(rows);
+        for (int i = 0; i < rows; i++) {
+            List<String> tr = new ArrayList<>(3);
+            tr.add(String.valueOf(i));
+            trs.add(tr);
+        }
     }
 
     @Override
     public void printing(ByteCode byteCode) {
 
+    }
+
+    @Override
+    public int rowsHexLength() {
+        return 2;
+    }
+
+    @Override
+    public List<String> th() {
+        return Arrays.asList("Nr.", "Bootstrap Method", "Arguments");
+    }
+
+    @Override
+    public List<List<String>> trs() {
+        return this.trs;
     }
 }
