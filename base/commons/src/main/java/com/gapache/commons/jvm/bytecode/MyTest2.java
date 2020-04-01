baaff7e3e0586c2284952b3cdc21575878c156bb
IsSenHu <1178515826@qq.com>
2020-04-01 17:55:41 +0800

    change someting

--- base/commons/src/main/java/com/gapache/commons/jvm/bytecode/MyTest2.java
+++ base/commons/src/main/java/com/gapache/commons/jvm/bytecode/MyTest2.java
@@ -1,6 +1,11 @@
 package com.gapache.commons.jvm.bytecode;
 
 /**
+ * java verbose -p(打印私有信息)
+ *
+ * moniterenter
+ * moniterexit
+ *
  * @author HuSen
  * create on 2020/3/23 3:45 下午
  */
@@ -12,6 +17,8 @@ public class MyTest2 {
 
     public static Integer in = 10;
 
+    private final Object object = new Object();
+
     public static void main(String[] args) {
         MyTest2 myTest2 = new MyTest2();
 
@@ -20,7 +27,17 @@ public class MyTest2 {
         in = 20;
     }
 
-    public void setX(int x) {
+    private synchronized void setX(int x) {
         this.x = x;
+    }
+
+    private void test(String str) {
+        synchronized (this.object) {
+            System.out.println("hello world");
+        }
+    }
+
+    private synchronized static void test2() {
+
     }
 }
