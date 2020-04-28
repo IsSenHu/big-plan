6dd447b2897167c4199f0c4873a684b4ff865a7d
IsSenHu <1178515826@qq.com>
2020-04-28 17:23:24 +0800

    ok

--- base/commons/src/main/java/com/gapache/commons/jvm/memory/MyTest5.java
+++ base/commons/src/main/java/com/gapache/commons/jvm/memory/MyTest5.java
@@ -12,12 +12,12 @@ public class MyTest5 {
         // jmap -heap pid
         // jstat -gc pid
 
-        // jcmd（conJDK 1.7开始增加的命令）
+        // jcmd（从JDK 1.7开始增加的命令）
         // jcmd pid help 列出当前的java进程可以执行的操作
         // jcmd pid help operation 查看具体命令的选项
         // jcmd pid VM.flags 查看VM启动参数
         // jcmd pid PerfCounter.print 查看JVM性能相关的一些参数
-        // jcmd pid VM.uptime 查看JVM的启动时长
+        // jcmd pid VM.uptime 查看JVM的运行时长
         // jcmd pid GC.class_histogram 查看系统中类的统计信息
         // jcmd pid Thread.print 查看线程的堆栈信息
         // jcmd pid GC.heap_dump filename 导出Heap dump文件，导出的文件可以通过jvisualvm查看
