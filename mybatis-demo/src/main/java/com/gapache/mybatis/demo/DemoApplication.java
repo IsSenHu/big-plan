6375866ca813504c0eeb8de50d89cc5572704750
胡森 <sensen@husens-iMac-Pro.local>
2020-05-12 23:13:48 +0800

    ok

---
+++ mybatis-demo/src/main/java/com/gapache/mybatis/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.gapache.mybatis.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author HuSen
+ * create on 2020/5/12 23:07
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
