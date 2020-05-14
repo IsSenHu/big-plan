20a9c0d4d91c91af27655c08aeb48e9a2cbcddec
IsSenHu <1178515826@qq.com>
2020-05-14 18:21:44 +0800

    ok

---
+++ mybatis-demo/src/main/java/com/gapache/mybatis/demo/dao/test/DemoConfiguration.java
@@ -0,0 +1,27 @@
+package com.gapache.mybatis.demo.dao.test;
+
+import com.gapache.mybatis.demo.dao.po.OrderJPO;
+import com.gapache.mybatis.demo.dao.po.OrderPO;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author HuSen
+ * create on 2020/5/14 6:17 下午
+ */
+@Configuration
+public class DemoConfiguration {
+
+    @Bean
+    public OrderPO po() {
+        System.out.println("po");
+        return new OrderPO();
+    }
+
+    @Bean
+    public OrderJPO jpo() {
+        po();
+        return new OrderJPO();
+    }
+
+}
