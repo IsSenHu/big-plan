8ad9d2f2b902e0b0668da8979d643db57e2a74c7
IsSenHu <1178515826@qq.com>
2020-05-27 15:48:27 +0800

    oko

--- base/commons/src/main/java/com/gapache/commons/proxy/ProxyDemo.java
+++ base/commons/src/main/java/com/gapache/commons/proxy/ProxyDemo.java
@@ -121,7 +121,7 @@ class TargetResultFixed implements FixedValue {
 }
 
 /**
- * 回调方法过滤
+ * 回调方法过滤器
  * 在CGLib回调时可以设置对不同方法执行不同的回调逻辑，或者根本不执行回调。
  * 在JDK动态代理中并没有类似的功能，对InvocationHandler接口方法的调用对代理类内的所以方法都有效。
  */
