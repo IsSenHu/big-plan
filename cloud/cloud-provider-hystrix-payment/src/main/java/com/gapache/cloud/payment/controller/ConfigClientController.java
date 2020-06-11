a9bf0996fb7bbaf7817a0b85410c6556c6350410
IsSenHu <1178515826@qq.com>
2020-06-11 10:58:45 +0800

    ok

--- cloud/cloud-provider-hystrix-payment/src/main/java/com/gapache/cloud/payment/controller/ConfigClientController.java
+++ cloud/cloud-provider-hystrix-payment/src/main/java/com/gapache/cloud/payment/controller/ConfigClientController.java
@@ -2,6 +2,7 @@ package com.gapache.cloud.payment.controller;
 
 import com.gapache.commons.model.JsonResult;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author HuSen
  * @since 2020/6/11 10:44 上午
  */
+@RefreshScope
 @RestController
 @RequestMapping("/api/config")
 public class ConfigClientController {
