755daa70b6e51f0dae94b8f8abd53f6d3ade2a28
unknown <1178515826@qq.com>
2020-06-24 09:46:35 +0800

    'ok'

--- base/commons/src/main/java/com/gapache/commons/model/BusinessException.java
+++ base/commons/src/main/java/com/gapache/commons/model/BusinessException.java
@@ -7,12 +7,12 @@ import lombok.Getter;
  * create on 2020/1/14 14:25
  */
 @Getter
-public class IException extends RuntimeException {
+public class BusinessException extends RuntimeException {
     private static final long serialVersionUID = -8570405796076634812L;
 
     private Error error;
 
-    public IException(Error error) {
+    public BusinessException(Error error) {
         super(null, null, false, false);
         this.error = error;
     }
