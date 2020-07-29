f010e513ed79630f86d19b4e63531c60c3828fe9
IsSenHu <1178515826@qq.com>
2020-07-29 19:57:45 +0800

    aqs

---
+++ base/commons/src/main/java/com/gapache/commons/lock/AbstractGapacheQueuedSynchronizer.java
@@ -0,0 +1,11 @@
+package com.gapache.commons.lock;
+
+/**
+ * 抽象的队列的同步器就是我们常说的aqs
+ *
+ * @author HuSen
+ * @since 2020/7/29 7:55 下午
+ */
+public abstract class AbstractGapacheQueuedSynchronizer extends AbstractGapacheOwnableSynchronizer {
+    private static final long serialVersionUID = -8361510769229983785L;
+}
