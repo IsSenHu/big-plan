4757834414f358caea1292d50d2e894840dee250
IsSenHu <1178515826@qq.com>
2020-07-30 22:18:18 +0800

    auth center

---
+++ ac/auth-center-server/src/main/java/com/gapache/cloud/auth/server/AuthCenterServer.java
@@ -0,0 +1,16 @@
+package com.gapache.cloud.auth.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author HuSen
+ * @since 2020/7/30 10:15 下午
+ */
+@SpringBootApplication
+public class AuthCenterServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthCenterServer.class, args);
+    }
+}
