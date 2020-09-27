f5fab981738a7c830b4be601d2fb9199417e7c06
IsSenHu <1178515826@qq.com>
2020-09-27 19:12:16 +0800

    change db url

--- ac/auth-center-server/src/main/java/com/gapache/cloud/auth/server/AuthCenterServer.java
+++ ac/auth-center-server/src/main/java/com/gapache/cloud/auth/server/AuthCenterServer.java
@@ -1,6 +1,8 @@
 package com.gapache.cloud.auth.server;
 
+import com.gapache.cloud.auth.server.service.UserService;
 import com.gapache.security.annotation.EnableAuthResourceServer;
+import com.gapache.security.model.UserDTO;
 import com.netflix.config.DynamicIntProperty;
 import com.netflix.config.DynamicPropertyFactory;
 import com.netflix.config.DynamicStringProperty;
@@ -8,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * @author HuSen
@@ -20,7 +23,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class AuthCenterServer {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthCenterServer.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(AuthCenterServer.class, args);
 
         DynamicPropertyFactory dynamicPropertyFactory = DynamicPropertyFactory.getInstance();
         DynamicIntProperty authCenterId = dynamicPropertyFactory
@@ -28,5 +31,13 @@ public class AuthCenterServer {
         DynamicStringProperty authCenterName = dynamicPropertyFactory
                 .getStringProperty("auth.center.name", "not setting");
         log.info("{}:{} is started.", authCenterId.get(), authCenterName.get());
+
+        try {
+            UserService userService = applicationContext.getBean(UserService.class);
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUsername("husen");
+            userDTO.setPassword("123456");
+            userService.create(userDTO);
+        } catch (Exception ignored) {}
     }
 }
