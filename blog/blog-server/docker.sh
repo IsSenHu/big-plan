e3bc175c17c8b3ddb1d2ba19b1b69e81fd46106e
IsSenHu <1178515826@qq.com>
2020-09-28 14:11:40 +0800

    ok

---
+++ blog/blog-server/docker.sh
@@ -0,0 +1,27 @@
+pwd
+LOG_PATH=/export/logs/blog-server
+SERVER_NAME=husen/blog-server
+cd blog/blog-server || exit
+# 容器ID
+CID=$(docker ps | grep "$SERVER_NAME" | awk '{print $1}')
+if [ -n "$CID" ]; then
+    echo "存在镜像在运行中，停止郑州运行的镜像$CID"
+    docker stop "$CID"
+fi
+IID=$(docker images | grep "$SERVER_NAME" | awk '{print $3}')
+if [ -n "$IID" ]; then
+    echo "存在$SERVER_NAME镜像，IID=$IID"
+    echo "移除旧的镜像"
+    docker rmi -f $SERVER_NAME
+fi
+echo "构建新的镜像$SERVER_NAME"
+docker build -t $SERVER_NAME .
+echo "所有的镜像"
+docker images
+echo "运行docker容器"
+docker run --name $SERVER_NAME -v $LOG_PATH:$LOG_PATH -d -p 10003:10003 $SERVER_NAME
+
+
+
+
+
